ec2016ef9900718756d6a522aee049ac6cedc7e3
Mina Nabil <mina9492@hotmail.com>
2018-06-02 20:28:43 +0200

    Student API

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'database', 'upload', 'MY_Main');
+$autoload['libraries'] = array('session', 'database', 'upload');
 
 /*
 | -------------------------------------------------------------------
