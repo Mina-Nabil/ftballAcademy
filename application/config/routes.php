e9703a4d063e564647a7359c83dc01bf298f2f3f
Mina Nabil <mina9492@hotmail.com>
2018-06-08 18:13:13 +0200

    Class Add

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,7 +58,7 @@ $route['Api/getStudents/(:num)'] = 'Students/getStudents/$1';
 //Classes API
 $route['Api/getClasses']        = 'Classes/getClasses';
 $route['Api/getClassesRoutes']  = 'Classes/getRoutes';
-$route['Api/addClass']          = 'Classes/addClass'
+$route['Api/addClass']          = 'Classes/addClass';
 
 
 $route['default_controller'] = 'welcome';
