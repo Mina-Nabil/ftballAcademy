5f15cbf761a1e4c45f8e54b030604485d29f500f
Mina Nabil <mina9492@hotmail.com>
2018-06-08 17:11:46 +0200

    Class Add

--- application/controllers/Classes.php
+++ application/controllers/Classes.php
@@ -6,7 +6,7 @@
     public function __construct()
     {
       header('Access-Control-Allow-Origin: *');
-      header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+      header("Access-Control-Allow-Headers: Access-Control-*, Origin, X-Requested-With, Content-Type, Accept");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
       $method = $_SERVER['REQUEST_METHOD'];
       if($method == "OPTIONS") {
