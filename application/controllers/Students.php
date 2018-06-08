d8d295c43765d90dd04217d3d84ae19f8a5554fc
Mina Nabil <mina9492@hotmail.com>
2018-06-08 18:11:37 +0200

    Class Add

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -5,11 +5,11 @@
 
     public function __construct()
     {
-
-      parent::__construct();
       header('Access-Control-Allow-Origin: *');
       header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+      parent::__construct();
+
     }
 
     public function getAllStudents($key=''){
