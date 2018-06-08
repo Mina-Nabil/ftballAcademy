50468ba47ad5524bef3e26f33d3dfb60fd988473
Mina Nabil <mina9492@hotmail.com>
2018-06-08 18:09:34 +0200

    Class Add

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -5,13 +5,7 @@
 
     public function __construct()
     {
-      header('Access-Control-Allow-Origin: *');
-      header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-      $method = $_SERVER['REQUEST_METHOD'];
-      if($method == "OPTIONS") {
-          die();
-      }
+
       parent::__construct();
       //Codeigniter : Write Less Do More
     }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,10 +8,7 @@
       header('Access-Control-Allow-Origin: *');
       header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-      $method = $_SERVER['REQUEST_METHOD'];
-      if($method == "OPTIONS") {
-          die();
-      }
+
       parent::__construct();
       //Codeigniter : Write Less Do More
     }
