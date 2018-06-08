3448d7dd3402cde15fc480e3878710c1b50499b3
Mina Nabil <mina9492@hotmail.com>
2018-06-08 18:10:34 +0200

    Class Add

--- application/controllers/Classes.php
+++ application/controllers/Classes.php
@@ -5,9 +5,11 @@
 
     public function __construct()
     {
-  
+
       parent::__construct();
-      //Codeigniter : Write Less Do More
+      header('Access-Control-Allow-Origin: *');
+      header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     }
 
     public function getClasses($key=''){
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,9 +5,7 @@
     public function __construct()
     {
 
-      header('Access-Control-Allow-Origin: *');
-      header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 
       parent::__construct();
       //Codeigniter : Write Less Do More
