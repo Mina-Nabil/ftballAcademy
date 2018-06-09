a18a4a78f0bed127b814fcd8d8b79800adc9ac18
Mina Nabil <mina9492@hotmail.com>
2018-06-09 13:46:02 +0200

    Get Class by ID

--- application/models/Classes_model.php
+++ application/models/Classes_model.php
@@ -51,6 +51,10 @@ class Classes_model extends CI_Model{
                         `CLSS_ID`    = ?";
           $inputs = array($Name, $Desc, $Year, $ID);
           $query = $this->db->query($strSQL, $inputs);
+          $strSQL = "SELECT CLSS_ID, CLSS_NAME, CLSS_DESC, CLSS_YEAR
+                    FROM Classes WHERE CLSS_ID = LAST_INSERT_ID() ";
+          $query = $this->db->query($strSQL);
+          return $query->result_array()[0];
 
         }
 
