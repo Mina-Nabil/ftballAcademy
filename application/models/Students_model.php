265783ae64a02f94f7575eec5b004053ce696980
Mina Nabil <mina9492@hotmail.com>
2018-06-12 20:14:23 +0200

    Positions API

--- application/models/Students_model.php
+++ application/models/Students_model.php
@@ -11,11 +11,11 @@ class Students_model extends CI_Model{
 
         public function getStudents(){
 
-          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLASS_ID, STUD_PRNT_TELL,
+          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLSS_ID, STUD_PRNT_TELL,
                             STUD_PRNT_NAME, STUD_MNTR_NAME, STUD_PREV_CLUB, STUD_FAV_POS,STUD_WGHT, STUD_LGTH,
                             CLSS_NAME, CLSS_YEAR, POST_NAME, POST_ABB
                       FROM Students, classes, Positions
-                      WHERE STUD_CLASS_ID = CLSS_ID
+                      WHERE STUD_CLSS_ID = CLSS_ID
                       AND   STUD_FAV_POS = POST_ID ";
           $query = $this->db->query($strSQL);
           return $query->result_array();
@@ -24,11 +24,11 @@ class Students_model extends CI_Model{
 
         public function getStudent_byID($ID){
 
-          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLASS_ID, STUD_PRNT_TELL,
+          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLSS_ID, STUD_PRNT_TELL,
                             STUD_PRNT_NAME, STUD_MNTR_NAME, STUD_PREV_CLUB, STUD_FAV_POS, STUD_WGHT, STUD_LGTH,
                             CLSS_NAME, CLSS_YEAR, POST_NAME, STUD_ACCS_CODE,  POST_ABB
                       FROM Students, classes, Positions
-                      WHERE STUD_CLASS_ID = CLSS_ID
+                      WHERE STUD_CLSS_ID = CLSS_ID
                       AND   STUD_FAV_POS = POST_ID
                       AND   STUD_ID = {$ID}";
           $query = $this->db->query($strSQL);
@@ -38,13 +38,13 @@ class Students_model extends CI_Model{
 
         public function getStudent_byClass($ID){
 
-          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLASS_ID, STUD_PRNT_TELL,
+          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLSS_ID, STUD_PRNT_TELL,
                             STUD_PRNT_NAME, STUD_MNTR_NAME, STUD_PREV_CLUB, STUD_FAV_POS, STUD_WGHT, STUD_LGTH,
                             CLSS_NAME, CLSS_YEAR, POST_NAME, STUD_ACCS_CODE
                       FROM Students, classes, Positions
-                      WHERE STUD_CLASS_ID = CLSS_ID
+                      WHERE STUD_CLSS_ID = CLSS_ID
                       AND   STUD_FAV_POS = POST_ID
-                      AND   STUD_CLASS_ID = {$ID}";
+                      AND   STUD_CLSS_ID = {$ID}";
           $query = $this->db->query($strSQL);
           return $query->result_array();
 
@@ -53,7 +53,7 @@ class Students_model extends CI_Model{
 
         public function insertStudent($Name, $Tel, $BirthDate, $ParentTel, $ClassID, $ParentTel2, $ParentName, $MentorName, $PrevClub, $FavPos, $Weight, $Length){
             //NN Text ArabicName Name DistrictID
-          $strSQL = "INSERT INTO Students (STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLASS_ID, STUD_PRNT_TELL, STUD_PRNT_NAME,
+          $strSQL = "INSERT INTO Students (STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLSS_ID, STUD_PRNT_TELL, STUD_PRNT_NAME,
                                            STUD_MNTR_NAME, STUD_PREV_CLUB, STUD_FAV_POS, STUD_WGHT, STUD_LGTH)
                      VALUES               (?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?)";
 
@@ -69,7 +69,7 @@ class Students_model extends CI_Model{
                         STUD_TEL   = ?,
                         STUD_BD   = ?,
                         STUD_PRNT_TEL    = ?,
-                        STUD_CLASS_ID   = ?,
+                        STUD_CLSS_ID   = ?,
                         STUD_PRNT_TELL   = ?,
                         STUD_PRNT_NAME   = ?,
                         STUD_MNTR_NAME   = ?,
