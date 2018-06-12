1d1e6c946227980b8aee180486a20a4f1d8aea31
Mina Nabil <mina9492@hotmail.com>
2018-06-12 21:00:36 +0200

    Positions API

--- application/models/Students_model.php
+++ application/models/Students_model.php
@@ -59,7 +59,15 @@ class Students_model extends CI_Model{
 
           $inputs = array($Name, $Tel, $BirthDate, $ParentTel, $ClassID, $ParentTel2, $ParentName, $MentorName, $PrevClub, $FavPos, $Weight, $Length);
           $query = $this->db->query($strSQL, $inputs);
-
+          $strSQL = "SELECT STUD_ID, STUD_NAME, STUD_TEL, STUD_BD, STUD_PRNT_TEL, STUD_CLSS_ID, STUD_PRNT_TELL,
+                            STUD_PRNT_NAME, STUD_MNTR_NAME, STUD_PREV_CLUB, STUD_FAV_POS, STUD_WGHT, STUD_LGTH,
+                            CLSS_NAME, CLSS_YEAR, POST_NAME, STUD_ACCS_CODE,  POST_ABB
+                      FROM Students, classes, Positions
+                      WHERE STUD_CLSS_ID = CLSS_ID
+                      AND   STUD_FAV_POS = POST_ID
+                      AND   STUD_ID = LAST_INSERT_ID()";
+          $query = $this->db->query($strSQL);
+          return $query->result_array();
         }
 
         public function editStudent($ID, $Name, $Tel, $BirthDate, $ParentTel, $ClassID, $ParentTel2, $ParentName, $MentorName, $PrevClub, $FavPos, $Weight, $Length){
