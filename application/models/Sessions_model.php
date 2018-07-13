620c2eb724a2e06e712b185a728f20b345854581
Mina Nabil <mina9492@hotmail.com>
2018-07-13 17:47:50 +0200

    Calendar

--- application/models/Sessions_model.php
+++ application/models/Sessions_model.php
@@ -31,7 +31,7 @@ class Sessions_model extends CI_Model{
 
         public function getSessions_limit($months){
           $strSQL = "SELECT SESS_ID, MOD(SESS_ID, 7) as color, SESS_STRT_DATE as start , CONCAT(SESS_DESC, 'Teams: ', GROUP_CONCAT(CLSS_NAME SEPARATOR ', ')) as title, SESS_END_DATE as 'end'
-                      FROM Sessions, Classes, SessionClass
+                      FROM Sessions, Classes, session_class
                       WHERE
                            SSCL_SESS_ID = SESS_ID
                       AND  SSCL_CLSS_ID = CLSS_ID
