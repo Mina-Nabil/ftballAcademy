57cd583381546fad28ad8d4c3ac7693947bd41fd
Nabil <Mina.Nabil@emc.com>
2018-07-22 14:08:56 +0200

    Attendance_Function

--- application/controllers/Sessions.php
+++ application/controllers/Sessions.php
@@ -37,6 +37,12 @@
       }
     }
 
+    public function takeAttendance($StudentID, $key=''){
+      if($this->permitApiCall($key)){
+        echo json_encode($this->Attendance_model->$StudentID($Month), JSON_UNESCAPED_UNICODE);
+      }
+    }
+
     public function addSession($key=''){
       if($this->permitApiCall($key)){
         $data = json_decode(file_get_contents('php://input'), true);
