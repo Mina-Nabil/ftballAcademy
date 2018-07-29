1871f42ed67c9d8aa5815cb97a1c23e46e9f1aaa
Mina Nabil <mina9492@hotmail.com>
2018-07-29 22:32:21 +0200

    Login

--- application/config/jwt.php
+++ application/config/jwt.php
@@ -1,5 +1,5 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-$config['jwt_key'] = 'ingDLMRuGe9UKHRNjs7cYckS2yul4lc3';
+$config['jwt_key'] = 'ingDLKRuAe9UHHRNcs7cLckS2pol2lc3'; 
 /*Generated token will expire in 1 minute for sample code
 * Increase this value as per requirement for production
 */
