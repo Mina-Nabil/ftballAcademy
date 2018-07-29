66ea0aec3ebd10757fffdf1fdf421b59fa7b0cea
Mina Nabil <mina9492@hotmail.com>
2018-07-29 22:49:00 +0200

    Login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('form', 'url', 'jwt', "authorization");
+$autoload['helper'] = array('form', 'url', 'jwt', "authorization", 'date');
 
 /*
 | -------------------------------------------------------------------
