6fba9b712aed65e61637ea31dea5bb2f3fcda92a
Mina Nabil <mina9492@hotmail.com>
2018-07-31 22:06:48 +0200

    Attendance Response

--- application/models/Attendance_model.php
+++ application/models/Attendance_model.php
@@ -27,7 +27,7 @@ class Attendance_model extends CI_Model{
         public function takeattendance($StudentBarcode){
           $Date = new DateTime(null, new DateTimeZone('Africa/Cairo'));
           $Session = $this->getCurrentSession($StudentBarcode);
-          if ($Session['res'] == 0) return 'Unavailable';
+          if ($Session['res'] == 0) return return array('result' => 0);;
           else if($Session['res'] == 0) return 0;
           else {
             $Start = new DateTime($Session['class']['SESS_STRT_DATE']);
@@ -40,7 +40,7 @@ class Attendance_model extends CI_Model{
             }else {
               $this->editAttendance($Session['class']['SESS_ID'], $Session['class']['STUD_ID'],1, $Date->format('Y-m-d H:i:s'), $Dur2->format('%H:%i:%s'));
             }
-            return 'taken';
+            return array('result' => 1);
           }
 
 
