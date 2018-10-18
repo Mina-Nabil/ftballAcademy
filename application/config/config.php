c728eee475c4976f1c47b706e8eaabdf34e9b5d5
Mina Nabil <mina9492@hotmail.com>
2018-10-18 15:44:04 +0200

    Alias

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/STMaryBE';
+$config['base_url'] = 'http://18.184.195.226/STMaryBE';
 
 /*
 |--------------------------------------------------------------------------
