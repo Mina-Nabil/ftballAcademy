91d713aa11158da8fc7612211d175b3867527df5
Mina Nabil <mina9492@hotmail.com>
2018-10-19 03:37:27 +0200

    Alias

--- application/models/Attendance_model.php
+++ application/models/Attendance_model.php
@@ -35,7 +35,9 @@ class Attendance_model extends CI_Model{
             $Dur1 = date_diff($End, $Start);
             $Dur2 = date_diff($End, $Date);
             echo $Date->format('Y-m-d H:i:s');
-            echo $Start->format('Y-m-d H:i:s');
+            echo '<br>' . $Start->format('Y-m-d H:i:s');
+            echo '<br>' . $Dur1->format('H:i:s');
+            echo '<br>' . $Dur1->format('H:i:s');
             if($Date < $Start) {
 
               $this->editAttendance($Session['class']['SESS_ID'], $Session['class']['STUD_ID'],1, $Date->format('Y-m-d H:i:s'), $Dur1->format('%H:%i:%s'));
