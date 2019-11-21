3c497046208c43df2754207351f5ffddcfa7ebae
Mina Nabil <mina9492@hotmail.com>
2019-11-21 20:58:03 +0200

    Removing noisy ads

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,7 +9,7 @@
       header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
       parent::__construct();
-
+      $this->output->set_header('Cache-Control: no-transform');
     }
 
     public function getUsers($key=''){
